524da6a0bd2d1fed37a15f4ed3404d955354e00c
Donald Lee <51865552+DonaldKLee@users.noreply.github.com>
2020-11-16 23:44:06 -0800

    Added comments to the code

--- article_to_audio.py
+++ article_to_audio.py
@@ -1,94 +1,120 @@
 """
 Refer to https://towardsdatascience.com/easy-text-to-speech-with-python-bfb34250036e
 """
-
+#Imports the following modules used in this function
 import os, requests, random 
+#Imports the Google Text to Speak module, which converts text into an mp3 file
 from gtts import gTTS
+#Imports a module for web scrapping =
 from bs4 import BeautifulSoup
 
 #A function that converts the article into an mp3 file
 def main(URL, audio_file_name):
+  #Goes to the following URL
   page = requests.get(URL)
 
+  #Gets the website's HTML code
   soup = BeautifulSoup(page.content, 'html.parser')
 
+  #CTV currently has a current and beta website, the current website headline has a class called "articleHeadline", but if an error occurs while trying to get the headline, it runs the code to get it from the class called "inner-headline"
   try:
-    article_title = soup.select('h1.articleHeadline')[0].text.strip()
+    #Gets the article's heading from CTV's current website
+    article_title = soup.select('h1.articleHeadline')[0].text.strip(" ")
   except:
-    article_title = soup.select('h1.inner-headline')[0].text.strip()
+    #Gets the article's heading from CTV's beta website
+    article_title = soup.select('h1.inner-headline')[0].text.strip(" ")
 
+  #Initalizes the name of the author incase it is not specified 
   author = "CTV"
 
-  paragraphs = soup.find_all('p')
-
+  #Gets all the div, p, a, span, and footer tags from the HTML code
   divs = soup.find_all('div')
+  paragraphs = soup.find_all('p')
+  a_links = soup.find_all('a')
+  spans = soup.find_all('span')
+  footers = soup.find_all('footer')
 
+  #Initalizes a list that will contain all the reccomended articles
   reccomended_articles = []
 
+  #A function that add non empty list items from "unfiltered_reccomended_articles" to "reccomended_articles"
   def filter_text():
     for reccomended_article in unfiltered_reccomended_articles:
       if len(reccomended_article) > 0:
+        #Removes additional spaces and adds that item to the list called "reccomended_articles"
         reccomended_articles.append(reccomended_article.strip(" "))
 
+  #For each div element in the HTML code, run the following
   for div in divs:
     try:
+      #If the div has a class called "sideItems", remove unnecessary new lines, and split it into a list
       if div.has_attr('class') and div['class'][0] == "sideItems":
           unfiltered_reccomended_articles = div.text.strip("\n ").split("\n")
+          #Runs a function called "filter_text"
           filter_text()
-      
+
+      #Else if the div has a class called "content-secondary", remove unnecessary new lines, and split it into a list
       elif div.has_attr('class') and div['class'][0] == "content-secondary":
         unfiltered_reccomended_articles = div.text.strip("\n ").split("\n")
         filter_text()
 
+    #If an error occurs, ignore it
     except:
       pass
 
-  footers = soup.find_all('footer')
-
   for footer in footers:
+    #Gets the text in the footer, remove unnecessary new lines, and split it into a list
     footer_text = footer.text.strip("\n ").split("\n")
 
-  a_links = soup.find_all('a')
-
+  #Loops through all the hyperlinks in the website
   for link in a_links:
+      #If the hyperlink has a class called "bioLink", set the variable "author" to the link text
       if link.has_attr('class') and link['class'][0] == "bioLink":
           author = link.text
   
-  spans = soup.find_all('span')
+  #Loops through all the span tags in the website
   for span in spans:
+    #If the span has a class called "bold", set the variable "author" to the span text
     if span.has_attr('class') and span['class'][0] == "bold":
       author = span.text
 
+  #Initalizes a list that will contain the contents of the article being web scrapped 
   article = []
 
-  #For p and a
+  #A list that contains p and a tag classes that will be excluded from the audio file (These classes generally contain off-topic content)
   excluded_classes = ["title", "ad-below", "bioLink", "back-top", "newWindow", "socialBio", "contactBio", "socialShareLabel"]
 
-
+  #Loops through each paragraph in the article
   for paragraph in paragraphs:
-      a_test = paragraph.find("a")
-      if paragraph.text.strip("\n ") in reccomended_articles:
+      #Gets all the hyperlinks in the paragraph
+      paragraph_a_links = paragraph.find("a")
+
+      #If the paragraph text is in the reccomended_articles or footer_text, ignore it
+      if paragraph.text.strip("\n ") in reccomended_articles or paragraph.text.strip("\n ") in footer_text:
         pass
 
-      elif paragraph.text in footer_text:
-        pass
-
-      elif a_test != None:
-
-          if a_test.has_attr('class') and a_test['class'][0] in excluded_classes:
+      #If a paragraph has a hyperlink, and the hyperlink's class is in the list called "excluded_classes", ignore it
+      elif paragraph_a_links != None:
+          if paragraph_a_links.has_attr('class') and paragraph_a_links['class'][0] in excluded_classes:
               pass
+            
           else:
+            #Else, add that paragraph to a list called "article"
               article.append(paragraph.text)
 
+      #If the paragraph has a class in the list called "excluded_classes", ignore it
       elif paragraph.has_attr('class') and paragraph['class'][0] in excluded_classes:
           pass
 
+      #If the word "SHARE" is in a paragraph, ignore it
       elif "SHARE" in paragraph.text:
           pass
 
+      #Else, add that paragraph into a list called "article"
       else:
           article.append(paragraph.text)
 
+  #Adds the article title, author, and combine all the article paragraphs from the list called "article" into a variable called "the_article"
   the_article = article_title + " \n\nThis article is by: " + author + "\n\n" + '\n'.join(article)
 
   #Sets the language and accent of the voice reading the mp3 file
@@ -96,22 +122,29 @@ def main(URL, audio_file_name):
 
   print("Converting text into audio...")
   
+  #Convert "the_article" into an mp3 file
   speech = gTTS(text = the_article, lang = language, slow = False)
 
   print("\nSaving text as an audio file...")
 
+  #Initalizes the following variables
   saved = False
   Attempts = 0
+
+  #If saved is false, or the number of attempts is 20, keep running.
   while saved == False or Attempts == 20:
       try:
           print("Attempt " + str(Attempts))
           Attempts += 1
+          #Saves the article as an mp3 file
           speech.save("static/audio/" + str(audio_file_name) + ".mp3")
+          #Sets saved to True
           saved = True
       except:
           pass
 
   print("\nThe text has been saved as an audio file called " + str(audio_file_name) + ".mp3")
 
+  #Returns the name of the mp3 file
   return str(audio_file_name)
 
--- main.py
+++ main.py
@@ -12,15 +12,18 @@ For the final project, you can make a program of your choice. You know how to do
 Refer to 
   - https://repl.it/talk/learn/Flask-Tutorial-Part-1-the-basics/26272
   - https://www.youtube.com/watch?v=Z1RJmh_OqeA
-
 """
-
+#Imports a python file called "article_to_audio.py"
 import article_to_audio
+#Imports the following modules
 import random, string, concurrent.futures, os
+#Imports Flask, a web framework
 from flask import Flask, render_template, url_for, request
+#Imports a module that allows us to detect the "MissingSchema" error
 from requests.exceptions import MissingSchema
 
-app = Flask(  # Create a flask app
+#Sets up the flask app
+app = Flask(
 	__name__,
 	template_folder='templates', #Name of folder containing html files
 	static_folder='static'  #Name of folder containing static files
@@ -41,42 +44,50 @@ def dated_url_for(endpoint, **values):
     return url_for(endpoint, **values)
 # ^ ^ ^
 
-@app.route('/')  # '/' for the default page
+#Sets up the home page
+@app.route('/')
 def base_page():
-  #Renders the base.html file (Renders the website)
+  #Renders the "base.html" file (Renders the website) and sends some arguments for the following variables
   return render_template('base.html', audio_file_name = "", audio_visibility = "none", loading_status = "none", invalid_url_state="none")
 
 #Runs the following when the convert button is pressed, or the user presses the enter key on the form
 @app.route('/', methods=['POST'])
 def my_form_post():
+    #Generates a random number for the name of the file
     audio_file_name = random.randint(1111111,9999999)
+    #Attempts the following until an error occurs
     try:
       #Retrieves the URL from the form
-      text = request.form['article_link']
+      link = request.form['article_link']
 
-      if "ctv" not in text:
+      #If "ctv" is not in the link, render the website again, but set the status of "invalid_url_state" to block (Visible)
+      if "ctv" not in link:
         return render_template('base.html', audio_file_name = "", audio_visibility = "none", loading_status = "none", invalid_url_state="block")
 
       else:
         #Allows mutiple functions to run concurrently (Ex - If there are 2+ users on the website)
         with concurrent.futures.ThreadPoolExecutor() as executor:
-          name_of_file = executor.submit(article_to_audio.main, text, audio_file_name).result()
-          print(name_of_file)
-          
+          #Runs the main function from "article_to_audio.py" and set the return varible to "name_of_file"
+          name_of_file = executor.submit(article_to_audio.main, link, audio_file_name).result()
+
+          print("I converted a file and named it " + str(name_of_file) + ".mp3")
+
+        #Render the website again, but set the status of "audio_visibility" to block (Visible)
         return render_template('base.html', audio_file_name = str(name_of_file), audio_visibility = "block", loading_status = "none", invalid_url_state="none")
       
-    #If the text the user enters is not a valid URL
+    #If the text the user enters is not a valid URL, render the website again, but set the status of "invalid_url_state" to block (Visible)
     except MissingSchema:
       return render_template('base.html', audio_file_name = "", audio_visibility = "none", loading_status = "none", invalid_url_state="block")
 
-    #If an error occurs while converting the article
+    #If an error occurs while converting the article, also render the website again, but set the status of "invalid_url_state" to block (Visible)
     except:
       return render_template('base.html', audio_file_name = "", audio_visibility = "none", loading_status = "none", invalid_url_state="block")
 
-if __name__ == "__main__":  # Makes sure this is the main process
+
+if __name__ == "__main__":  #Makes sure this is the main process
 	app.run( 
     debug=True,
-    # Starts the site
-		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
-		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+    #Starts the website
+		host='0.0.0.0',  #Sets the host, required for repl to detect the site
+		port=random.randint(2000, 9000)  #Randomly select the port the machine hosts on.
 	)
